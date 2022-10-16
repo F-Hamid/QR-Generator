0f41f46ea5eb6dc38f31e055043d0f681b58a2a3
F-Hamid <hamid.fateh81@gmail.com>
2022-10-16 20:18:34 +0100

    qrcode generated

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+let qrcode = new QRCode("qrcode");
+
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const empty = document.getElementById("delete");
+const generate = document.getElementById("generate");
+const loading = document.getElementById("loading");
+const qrdiv = document.getElementById("qrdiv");
+
+const addSpinner = () => {
+  generate.classList.remove("show");
+  loading.classList.add("show");
+  generate.classList.add("hide");
+  loading.classList.remove("hide");
+};
+
+const removeSpinner = () => {
+  generate.classList.remove("hide");
+  loading.classList.add("hide");
+  generate.classList.add("show");
+  loading.classList.remove("show");
+};
 
 const isValidUrl = (urlString) => {
   var urlPattern = new RegExp(
@@ -13,6 +32,23 @@ const isValidUrl = (urlString) => {
     "i"
   ); // validate fragment locator
   return !!urlPattern.test(urlString);
+};
+
+// GenerateQR
+const generateQR = (url) => {
+  setTimeout(() => {
+    let qrcode = new QRCode("qrcode", {
+      text: url,
+      width: 128,
+      height: 128,
+      colorDark: "#000000",
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
+    qrdiv.classList.remove("hide");
+    qrdiv.classList.add("show");
+    removeSpinner();
+  }, 1000);
 };
 
 const checkUrl = (url) => {
@@ -29,13 +65,24 @@ const checkUrl = (url) => {
 const onGenerate = (e) => {
   e.preventDefault();
   const url = input.value;
-  checkUrl(url);
-  console.log(checkUrl(url));
+  addSpinner();
+  //   Add delete button
+  empty.classList.remove("hide");
+  const message = checkUrl(url);
+  generateQR(url);
+  document.getElementById("tag-id").innerHTML = message;
+  console.log(message);
+  return url;
 };
 
 const onDelete = (e) => {
   e.preventDefault();
   input.value = "";
+  removeSpinner();
+  empty.classList.add("hide");
+  qrdiv.classList.remove("show");
+  qrdiv.classList.add("hide");
+  qrcode.clear();
 };
 
 form.addEventListener("submit", onGenerate);
