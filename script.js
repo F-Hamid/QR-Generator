5f960502da537d6e4efda8f2e4b4364b24e27a9c
F-Hamid <hamid.fateh81@gmail.com>
2022-10-17 11:05:51 +0100

    V 1.0.0

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const input = document.getElementById("input");
 const empty = document.getElementById("delete");
 const generate = document.getElementById("generate");
 const loading = document.getElementById("loading");
-const qrdiv = document.getElementById("qrdiv");
+const qrdiv = document.getElementById("qrcode");
 
 const addSpinner = () => {
   generate.classList.remove("show");
@@ -35,44 +35,67 @@ const isValidUrl = (urlString) => {
 };
 
 // GenerateQR
-const generateQR = (url) => {
-  setTimeout(() => {
-    let qrcode = new QRCode("qrcode", {
-      text: url,
-      width: 128,
-      height: 128,
-      colorDark: "#000000",
-      colorLight: "#ffffff",
-      correctLevel: QRCode.CorrectLevel.H,
-    });
-    qrdiv.classList.remove("hide");
-    qrdiv.classList.add("show");
-    removeSpinner();
-  }, 1000);
+const generateQR = async (url) => {
+  let qrcode = await new QRCode("qrcode", {
+    text: url,
+    width: 200,
+    height: 200,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H,
+  });
 };
 
 const checkUrl = (url) => {
-  if (url === "") {
-    return "Please enter your URL";
-  }
-  if (!isValidUrl(url)) {
-    return "Please enter a valid URL";
-  } else {
-    return "Success";
-  }
+  setTimeout(() => {
+    qrdiv.classList.remove("hide");
+    qrdiv.classList.add("show");
+
+    removeSpinner();
+    if (url === "") {
+      return (qrdiv.innerHTML = "Please enter your URL");
+    }
+    if (!isValidUrl(url)) {
+      return (qrdiv.innerHTML = "Please enter valid your URL");
+    } else {
+      clearQR();
+
+      generateQR(url);
+      setTimeout(() => {
+        const saveUrl = qrdiv.querySelector("img").src;
+        saveButton(saveUrl);
+      }, 100);
+    }
+  }, 1000);
+};
+
+const clearQR = () => {
+  qrdiv.innerHTML = "";
+  const saveBtn = document.getElementById("save-btn");
+  if (saveBtn) saveBtn.remove();
+};
+
+const saveButton = (saveUrl) => {
+  const button = document.createElement("a");
+  button.id = "save-btn";
+  button.classList =
+    "h-16 my-12 center w-72 text-[#eee] bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-3xl px-5 py-2.5 text-center mr-2 mb-2 shadow-sm hover:shadow-lg";
+  button.href = saveUrl;
+  button.download = "QR-code";
+  button.innerHTML = "save";
+  document.getElementById("qr-container").appendChild(button);
 };
 
 const onGenerate = (e) => {
   e.preventDefault();
+  //   Clear QR
+  qrcode.clear();
+
   const url = input.value;
   addSpinner();
   //   Add delete button
   empty.classList.remove("hide");
-  const message = checkUrl(url);
-  generateQR(url);
-  document.getElementById("tag-id").innerHTML = message;
-  console.log(message);
-  return url;
+  checkUrl(url);
 };
 
 const onDelete = (e) => {
@@ -82,7 +105,7 @@ const onDelete = (e) => {
   empty.classList.add("hide");
   qrdiv.classList.remove("show");
   qrdiv.classList.add("hide");
-  qrcode.clear();
+  clearQR();
 };
 
 form.addEventListener("submit", onGenerate);
